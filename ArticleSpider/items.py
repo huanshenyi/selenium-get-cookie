d3a7a9dd6789365e050fff31df1f7be0f5a32d93
huanshenyi <txy1226052@gmail.com>
2019-05-13 22:39:52 +0900

    itemloader

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -6,6 +6,9 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+
 
 
 class ArticlespiderItem(scrapy.Item):
@@ -13,10 +16,29 @@ class ArticlespiderItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
-class LagouJob(scrapy.Item):
+
+def remove_splash():
+    pass
+
+class LagouJobItemLoader(ItemLoader):
+    default_input_processor = TakeFirst()
+
+class LagouJobItem(scrapy.Item):
     #lagouwang
     title = scrapy.Field()
     url = scrapy.Field()
     url_object_id = scrapy.Field()
     salary = scrapy.Field()
     job_city = scrapy.Field()
+    work_years = scrapy.Field()
+    degree_need = scrapy.Field()
+    job_type = scrapy.Field()
+    publish_time = scrapy.Field()
+    job_advantage = scrapy.Field()
+    job_desc = scrapy.Field()
+    job_addr = scrapy.Field()
+    company_name = scrapy.Field()
+    company_url = scrapy.Field()
+    tags = scrapy.Field()
+    crawl_time = scrapy.Field()
+
--- ArticleSpider/spiders/lagou.py
+++ ArticleSpider/spiders/lagou.py
@@ -6,6 +6,10 @@ from selenium import webdriver
 import os
 import time
 import pickle
+from ArticleSpider.items import LagouJobItemLoader, LagouJobItem
+from utils.common import get_md5
+from datetime import datetime
+from scrapy.loader import ItemLoader
 
 
 class LagouSpider(CrawlSpider):
@@ -25,28 +29,30 @@ class LagouSpider(CrawlSpider):
     # def process_results(self, response, results):
     #     return results
 
-    #selenium使用,startの書き直し
+    # selenium使用,startの書き直し
     def start_requests(self):
-        #selenium使用してログイン,cookieを取得,scrapyのrequestに使用させる
-        #1.seleniumでログイン
+        # selenium使用してログイン,cookieを取得,scrapyのrequestに使用させる
+        # 1.seleniumでログイン
         # x.cookieをファイルから取得
         cookies = []
-        if os.path.exists(os.path.abspath(os.path.join(os.getcwd(), ".."))+'\cookies\lagou.cookic'):
-            cookies = pickle.load(open(os.path.abspath(os.path.join(os.getcwd(), ".."))+'\cookies\lagou.cookic',"rb"))
+        if os.path.exists(os.path.abspath(os.path.join(os.getcwd(), "..")) + '\cookies\lagou.cookic'):
+            cookies = pickle.load(
+                open(os.path.abspath(os.path.join(os.getcwd(), "..")) + '\cookies\lagou.cookic', "rb"))
         if not cookies:
-            driverPath = os.path.abspath(os.path.join(os.getcwd(), ".."))+'\driver\chromedriver.exe'
+            driverPath = os.path.abspath(os.path.join(os.getcwd(), "..")) + '\driver\chromedriver.exe'
             browser = webdriver.Chrome(executable_path=driverPath)
             browser.get('https://passport.lagou.com/login/login.html')
             userInput = browser.find_element_by_xpath("//div[@class='form_body']//input[@class='input input_white']")
             userInput.send_keys('')
-            pswdInput = browser.find_element_by_xpath("//div[@class='form_body']//input[@type='password']").send_keys('')
+            pswdInput = browser.find_element_by_xpath("//div[@class='form_body']//input[@type='password']").send_keys(
+                '')
             browser.find_element_by_xpath("//div[@class='form_body']//input[@type='submit']").click()
             time.sleep(10)
-            #2.cookie取得
+            # 2.cookie取得
             cookies = browser.get_cookies()
 
-            #3.cookieをファイルに書き込む
-            filePath = os.path.abspath(os.path.join(os.getcwd(), ".."))+'\cookies\lagou.cookic'
+            # 3.cookieをファイルに書き込む
+            filePath = os.path.abspath(os.path.join(os.getcwd(), "..")) + '\cookies\lagou.cookic'
             pickle.dump(cookies, open(filePath, "wb"))
         cookie_dict = {}
         for cookie in cookies:
@@ -55,11 +61,24 @@ class LagouSpider(CrawlSpider):
         for url in self.start_urls:
             yield scrapy.Request(url, dont_filter=True, cookies=cookie_dict)
 
-
-
     def parse_job(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        item_loader = LagouJobItemLoader(item=LagouJobItem(), response=response)
+        item_loader.add_css("title", ".job-name::attr(title)")
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("url_object_id", get_md5(response.url))
+        item_loader.add_xpath("salary", "//dd[@class='job_request']/p/span[@class='salary']/text()")
+        item_loader.add_xpath("job_city", "//*[@class='job_request']/p/span[2]/text()")
+        item_loader.add_xpath("work_years", "//*[@class='job_request']/p/span[3]/text()")
+        item_loader.add_xpath("degree_need", "//*[@class='job_request']/p/span[4]/text()")
+        item_loader.add_xpath("job_type", "//*[@class='job_request']/p/span[5]/text()")
+        item_loader.add_css("tags", ".position-label li::text")
+        item_loader.add_css("publish_time", ".publish_time::text")
+        item_loader.add_css("job_advantage", ".job-advantage p::text")
+        item_loader.add_xpath("job_desc", "//*[@class='job_bt']/div[@class='job-detail']")
+        item_loader.add_css("job_addr", ".work_addr")
+        item_loader.add_css("company_name", "#job_company dt a img::attr(alt)")
+        item_loader.add_css("company_url", "#job_company dt a::attr(href)")
+        item_loader.add_value("crawl_time", datetime.now())
+        job_item = item_loader.load_item()
+
+        return job_item
